772233f5bf6d0b3cff5f77f1c2387be426b2e59f
Chris Voxland <chris.voxland@jackpocket.com>
2024-02-29 12:59:45 -0800

    respect instance variable presence

--- lib/pg_ha_migrations/hacks/disable_ddl_transaction.rb
+++ lib/pg_ha_migrations/hacks/disable_ddl_transaction.rb
@@ -4,9 +4,8 @@ module PgHaMigrations
   module ActiveRecordHacks
     module DisableDdlTransaction
       def disable_ddl_transaction
-        return false unless PgHaMigrations.config.always_disable_ddl_transactions
         # Default to disabled unless someone has set it elsewhere
-        defined?(@disable_ddl_transaction) ? @disable_ddl_transaction : true
+        defined?(@disable_ddl_transaction) ? @disable_ddl_transaction : PgHaMigrations.config.always_disable_ddl_transactions
       end
     end
   end
