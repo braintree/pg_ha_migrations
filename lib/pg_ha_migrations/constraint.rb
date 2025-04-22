bca1058fc9771695afdc8ef085a1daa86537d5f8
James Coleman <jamecoleman@paypal.com>
2025-04-22 14:29:02 +0000

    Add idempotence and existing constraint re-use to safe_make_column_not_nullable
    If an existing constraint already exists we should re-use that. If it's
    not yet validated, we still need to validate it -- that gives use
    idempotency, and we no longer need to raise an error if the previous run
    failed. If the existing constraint is already validated, there's no need
    to validate it, and we can simply proceed.
    
    While it's true that _if you already know you have a validated
    constraint_ we idiomatically prefer using
    `safe_make_column_not_nullable_from_check_constraint` in cases where
    your database state might be unknown (e.g., a previous failure), it's
    still helpful to be able to use `safe_make_column_not_nullable`.
    
    Fixes #130
    Fixes #131

--- lib/pg_ha_migrations/constraint.rb
+++ lib/pg_ha_migrations/constraint.rb
@@ -1,1 +1,8 @@
-PgHaMigrations::CheckConstraint = Struct.new(:name, :definition, :validated)
+PgHaMigrations::CheckConstraint = Struct.new(:name, :definition, :validated) do
+  def initialize(name, definition, validated)
+    # pg_get_constraintdef includes NOT VALID in the definition,
+    # but we return that as a separate attribute.
+    definition = definition&.gsub(/ NOT VALID\Z/, "")
+    super(name, definition, validated)
+  end
+end
